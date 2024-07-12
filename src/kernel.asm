49f946e5a997bbc893ff159017eda2b05ff58bcc
rayyan21d <rayyan9290@gmail.com>
2024-07-12 17:34:19 +0530

    alignment issues fixed

---
+++ src/kernel.asm
@@ -0,0 +1,25 @@
+[BITS 32]
+
+global _start
+
+CODE_SEG equ 0x08
+DATA_SEG equ 0x10
+
+_start:
+    mov ax, DATA_SEG ; Data Segment
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+
+    ;Enabling the A20 Line using Fast A20 Gate
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    jmp $
+
+times 512-($-$$) db 0

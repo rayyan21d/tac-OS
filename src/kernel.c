cf40cbb85588f1309f22c1ecae741d60bdbd77a7
rayyan21d <rayyan9290@gmail.com>
2024-07-12 23:22:07 +0530

    hello world one

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,16 @@
 #include "kernel.h"
 
-void kernal_start()
+void kernel_main()
 {
+    char* video_mem = (char*)(0xB8000);
+    char msg[12] = "Hello World!";
 
-}
+    for(int i=0; i<12; i++){
+
+        video_mem[2*i] = msg[i];
+        video_mem[2*i+1] = 2;
+
+    }
+
+
+}   
